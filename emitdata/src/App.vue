d3ce910eb46bb16f04d9271327faa1dc8c046c24
binnazcabuk <binnazcabuk9@gmail.com>
2022-07-27 14:34:04 +0300

    $emit-data-send

---
+++ emitdata/src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+   <!-- child componentten "customChange"isimli bir event gelecek geldiğinde ilgili method çalışsın -->
+    <InputText @customChange="handleCustomChange"/>
+     <p>Uppercase: {{ uppercase }}</p>
+</template>
+
+<script>
+import InputText from './components/InputText.vue'
+
+
+export default {
+  name: 'App',
+  components: {
+    InputText
+},
+   data() {
+      return {
+        uppercase: ''
+      }
+    },
+    methods: {
+      //gelen veriyi al
+        handleCustomChange (s) {
+          this.uppercase = s
+        }
+    }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ emitdata/src/components/InputText.vue
@@ -0,0 +1,18 @@
+<template>
+ <div>
+    <!-- birşey girildiği an methods çalışsın -->
+    <input type="text" placeholder="Custom input!" @input="handleChange" />
+  </div>
+</template>
+
+<script>
+  export default {
+      methods: {
+          handleChange (event) {
+            // input ile girilen veriyi target üzerinden oku, ana component'e büyük harf dönüşümü yapılmış olarak  gönder
+            // $emit içerisinde ilk değer gönderim değişkeni,ikincisi ise gönderilecek veri
+              this.$emit("customChange", event.target.value.toUpperCase())
+          }
+      }
+  }
+</script>
