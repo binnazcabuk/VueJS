25990452eff59c9f49dccdffabebf97cb3a29282
binnazcabuk <binnazcabuk9@gmail.com>
2022-07-29 16:13:42 +0300

    .

--- vue-router/src/App.vue
+++ vue-router/src/App.vue
@@ -5,7 +5,9 @@
         <br />
         <Header />
         <hr />
+        <transition name="slide" mode="out-in">
         <router-view></router-view>
+        </transition>
       </div>
     </div>
   </div>
@@ -20,5 +22,35 @@ export default {
 };
 </script>
 <style >
+.slide-enter{
+  opacity: 0; 
+}
+.slide-enter-active{
+  animation: slide-in 1s ease-out forwards;
+  transition: opacity 0.5s;
+}
+.slide-leave-active{
+   animation: slide-out 1s ease-out forwards;
+  transition: opacity 1s;
+  opacity: 0;
+  position: absolute;
+}
 
+@keyframes slide-in{
+  from{
+    transform: translateY(20px);
+  }
+  to{
+    transform: translateY(0px);
+  }
+}
+
+@keyframes slide-out{
+  from{
+    transform: translateY(0px);
+  }
+  to{
+    transform: translateY(20px);
+  }
+}
 </style>
